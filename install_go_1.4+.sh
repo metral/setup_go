4bea29a0cdfeaa415534f37b8e635d0e1fb5e442
Mike Metral <metral@gmail.com>
2016-01-29 12:24:44 -0800

    Update install_go_1.4+.sh

--- install_go_1.4+.sh
+++ install_go_1.4+.sh
@@ -10,8 +10,8 @@ source $GVM_ROOT/scripts/gvm
 gvm install go$GOVERSION
 gvm use go$GOVERSION --default
 
-sed -i "s#export GVM_ROOT.*#export GVM_ROOT; GVM_ROOT=\"$HOME\"/.gvm#g" ~/.gvm/environments/default
-sed -i "s#export GOPATH.*#export GOPATH; GOPATH=\"$DIR\"#g" ~/.gvm/environments/default
+#sed -i "s#export GVM_ROOT.*#export GVM_ROOT; GVM_ROOT=\"$HOME\"/.gvm#g" ~/.gvm/environments/default
+#sed -i "s#export GOPATH.*#export GOPATH; GOPATH=\"$DIR\"#g" ~/.gvm/environments/default
 
 echo "==================================="
 echo "source ~/.bashrc"
