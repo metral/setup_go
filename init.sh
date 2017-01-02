6a355067e7aa58c2eb2f8fce1ae754ab2c1945b7
Mike Metral <metral@gmail.com>
2017-01-01 22:30:39 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,8 +4,8 @@ DIR="$HOME/go"
 GOVERSION=1.4
 
 mkdir $DIR
-echo "export GOPATH=\$(pwd):\$(pwd)/vendor" > ~/gbimports
-echo "ln -f -s linux-amd64/ pkg/linux-amd64" >> ~/gbimports
+#echo "export GOPATH=\$(pwd):\$(pwd)/vendor" > ~/gbimports
+#echo "ln -f -s linux-amd64/ pkg/linux-amd64" >> ~/gbimports
 export CGO_ENABLED=0
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 
